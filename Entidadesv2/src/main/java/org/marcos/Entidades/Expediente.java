6c67e94aad5cf689b290ef0b51841b66cd497878
rosa <natsukiarr@gmail.com>
2024-11-03 16:02:52 -0800

    ADD Default Values to diagnostico and enfermedadPrevia

--- Entidadesv2/src/main/java/org/marcos/Entidades/Expediente.java
+++ Entidadesv2/src/main/java/org/marcos/Entidades/Expediente.java
@@ -12,10 +12,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -75,5 +74,19 @@ public @Data class Expediente {
     
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "expediente")
     private List<MedicamentoDelExpediente> medicamentos;
+    
+    @PrePersist()
+    public void onPrePersist() {
+      
+        if (this.diagnostico == null) {
+            this.diagnostico = "N/A";
+        }
+        
+        if (this.enfermedadPrevia == null) {
+            this.enfermedadPrevia = "N/A";
+        }
+          
+    }
+    
     
 }
